b80afee7f4a7f35cbac4f6c6da68bf404c56be75
seosieve <royalcircle97@naver.com>
2024-05-22 19:15:10 +0900

    ADD: User Defaults

--- SeSAC3/ViewControllers/DiaryViewController.swift
+++ SeSAC3/ViewControllers/DiaryViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class DiaryViewController: UIViewController {
 
+    let userDefaults = UserDefaults.standard
     let labelTitle = ["幸せだよ", "大好き", "好きだよ", "慌てるよ", "心が痛む", "憂鬱だ", "退屈だ", "感情がない", "恋しい"]
     
     @IBOutlet var backgroundImageView: UIImageView!
@@ -28,13 +29,31 @@ class DiaryViewController: UIViewController {
             button.setImage(image, for: .normal)
             button.tag = i
         }
+        
+        for i in 0..<numberLabelCollection.count {
+            let label = numberLabelCollection[i]
+            label.tag = i
+            let count = userDefaults.integer(forKey: labelTitle[i])
+            
+            if count != 0 {
+                label.text = "\(labelTitle[i]) \(count)"
+            } else {
+                label.text = labelTitle[i]
+            }
+        }
     }
     
     //TapGesture -> Action
     @IBAction func slimeButtonPressed(_ sender: UIButton) {
-        guard let currentText = numberLabelCollection[sender.tag].text else { return }
-        let currentTextArr = currentText.split(separator: " ")
-        let number = (currentTextArr.count == 1 ? 0 : Int(currentTextArr[1])!)
+        let number = userDefaults.integer(forKey: labelTitle[sender.tag])
+        userDefaults.set(number+1, forKey: labelTitle[sender.tag])
         numberLabelCollection[sender.tag].text = "\(labelTitle[sender.tag]) \(number+1)"
+    }
+    
+    @IBAction func resetButtonPressed(_ sender: UIButton) {
+        for key in userDefaults.dictionaryRepresentation().keys {
+            userDefaults.removeObject(forKey: key.description)
+        }
+        setViews()
     }
 }
